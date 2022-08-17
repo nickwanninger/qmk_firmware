deada9e411039ddf5b173f65b3cad58c7c640539
Nick Wanninger <nickwanninger@gmail.com>
2022-08-17 01:00:16 -0400

    update planck

--- keyboards/planck/keymaps/ncw/keymap.c
+++ keyboards/planck/keymaps/ncw/keymap.c
@@ -27,7 +27,8 @@ enum planck_layers { _QWERTY, _LOWER, _RAISE, _ADJUST, _EMOD, _FN };
 
 enum custom_keycodes {
     // Keycode to enable the EMOD layer
-    KC_EMOD = SAFE_RANGE
+    KC_EMOD = SAFE_RANGE,
+    KC_NEWL, // type '\n'
 };
 
 // clang-format off
@@ -46,14 +47,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD, _______, _______, KC_LCBR, KC_RCBR, _______, _______, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL , _______,
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, _______, KC_PIPE,
-    _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______, _______, KC_NEWL,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 
 // Raise is meant for numbers and also contains some arrow keys so I don't have to move my hands so much.
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSPC,
+    KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL , KC_BSPC,
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0   , KC_BSLS,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
@@ -77,9 +78,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_FN] = LAYOUT_planck_grid(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, 
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10 , _______,
+    _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
@@ -100,6 +101,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 
     switch (keycode) {
+        case KC_NEWL:
+            if (record->event.pressed) SEND_STRING("\\n");
+            break;
         case KC_EMOD:
             if (record->event.pressed) {
                 emod_used = false;
